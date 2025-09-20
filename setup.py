a608a34226f1c09359376584a5621e1a2b609c61
LucasBonna <lucasbonnafavaro@gmail.com>
2025-09-20 14:36:33 -0300

    fix: update version to 0.6.0 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyNFe",
-    version="0.5.5",
+    version="0.6.0",
     author="TadaSoftware",
     author_email="tadasoftware@gmail.com",
     description="Interface library with the Brazilian Electronic Invoice web services",
